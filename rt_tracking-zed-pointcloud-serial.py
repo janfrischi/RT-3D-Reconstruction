e6b7bdfe13cef089ad57f7855a5d3feb94cee650
Jan Frischknecht <you@example.com>
2024-10-29 09:28:12 +0100

    Minor bug fixes

--- rt_tracking-zed-pointcloud-serial.py
+++ rt_tracking-zed-pointcloud-serial.py
@@ -3,7 +3,6 @@ import pyzed.sl as sl
 import cv2
 import time
 import torch
-from shapely.constructive import centroid
 
 from ultralytics import YOLO
 import open3d as o3d
@@ -19,6 +18,9 @@ color_map = {
     66: [255, 0, 0]  # Keyboard - red
 }
 
+# Define class names for the detected objects
+class_names = {0: "Person", 39: "Bottle", 41: "Cup", 62: "Laptop", 64: "Mouse", 66: "Keyboard"}
+
 def erode_mask(mask, iterations=1):
     kernel = np.ones((8, 8), np.uint8)
     eroded_mask = cv2.erode(mask, kernel, iterations=iterations)
@@ -27,16 +29,21 @@ def erode_mask(mask, iterations=1):
 # Convert 2D mask pixel coordinates to 3D points using depth values on GPU
 def get_3d_points_torch(mask_indices, depth_map, cx, cy, fx, fy, device='cuda'):
     mask_indices = torch.tensor(mask_indices, device=device)
+    # Extract the u and v coordinates from the mask indices
     u_coords = mask_indices[:, 1]
     v_coords = mask_indices[:, 0]
     depth_values = depth_map[v_coords, u_coords].to(device)
     valid_mask = depth_values > 0
+    # Extract the valid u, v, and depth values
     u_coords = u_coords[valid_mask]
     v_coords = v_coords[valid_mask]
     depth_values = depth_values[valid_mask]
+    # Calculate the x, y, and z coordinates of the 3D points using the camera intrinsics
+    # Where fx,fy are the focal lengths and cx,cy are the principal points
     x_coords = (u_coords - cx) * depth_values / fx
     y_coords = (v_coords - cy) * depth_values / fy
     z_coords = depth_values
+    # Return a tensor of shape (N, 3) representing the 3D points, stacked along the last dimension
     return torch.stack((x_coords, y_coords, z_coords), dim=-1)
 
 def random_sample_pointcloud(pc, fraction):
@@ -55,8 +62,6 @@ def random_sample_pointcloud(pc, fraction):
     indices = random.sample(range(n_points), sample_size)
     return pc[indices]
 
-def calculate_centroid(point_cloud):
-    return np.mean(point_cloud, axis=0)
 
 def main():
 
@@ -99,7 +104,8 @@ def main():
     cx, cy = calibration_params.left_cam.cx, calibration_params.left_cam.cy
     img_width, img_height = calibration_params.left_cam.image_size.width, calibration_params.left_cam.image_size.height
 
-    # Transformation matrices from camera frame to chess frame for camera 1 and camera 2
+    # Transformation matrices from the chessboard to the camera frames
+
     T_chess_cam1 = np.array([[-0.9770, 0.1087, -0.1835, 0.3060],
                              [-0.2100, -0.6401, 0.7390, -0.7103],
                              [-0.0371, 0.7605, 0.6482, -0.6424],
@@ -110,6 +116,8 @@ def main():
                              [-0.0068, 0.7615, 0.6481, -0.7279],
                              [0.0000, 0.0000, 0.0000, 1.0000]])
 
+    # Transformation matrix from the chessboard to the robot base frame
+
     T_robot_chess = np.array([[-1, 0, 0, 0.3580],
                               [0, 1, 0, 0.0300],
                               [0, 0, -1, 0.0060],
@@ -118,6 +126,10 @@ def main():
     # Transformation from camera frame (left camera of respective stereo camera) to robot base frame
     T_robot_cam1 = np.dot(T_robot_chess, T_chess_cam1)
     T_robot_cam2 = np.dot(T_robot_chess, T_chess_cam2)
+
+    # Create a robot base frame
+    robot_base_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.05, origin=[0, 0, 0])
+    robot_base_frame.transform(T_robot_cam1)
 
     # Create Mat object to hold the image and depth map
     image = sl.Mat()
@@ -130,21 +142,20 @@ def main():
     vis.create_window(window_name='Visualization-Pointcloud', width=img_width, height=img_height)
     vis.add_geometry(coordinate_frame)
 
+    # Create a named window for the video feed
     cv2.namedWindow("YOLO11 Segmentation+Tracking")
 
     fps_values = []
     frame_count = 0
     update_frequency = 5  # Update every 5 frames -> Significant performance improvement in visualization
 
-    # Main loop responsible for capturing and processing frames
+    # Real-Time Loop for Capturing and Processing Frames
     while key != ord('q'):
         start_time = time.time()
 
-        # Grab an image from the camera
+        # Image and Depth Capture
         if zed.grab() == sl.ERROR_CODE.SUCCESS:
             zed.retrieve_image(image, sl.VIEW.LEFT)
-
-            # Check if depth retrieval is successful
             depth_retrieval_result = zed.retrieve_measure(depth, sl.MEASURE.DEPTH)
             if depth_retrieval_result != sl.ERROR_CODE.SUCCESS:
                 print(f"Error retrieving depth: {depth_retrieval_result}")
@@ -152,7 +163,7 @@ def main():
 
             frame = image.get_data()
 
-            # Convert frame from 4 channels (RGBA) to 3 channels (BGR)
+            # Convert frame from 4 channels (RGBA) to 3 channels (BGR) -> OpenCV uses BGR format
             frame = cv2.cvtColor(frame, cv2.COLOR_BGRA2BGR)
 
             # Run YOLO11 inference on the frame
@@ -176,34 +187,37 @@ def main():
                 print("Error: Depth map is empty")
                 continue
 
-            # Visualize the results on the frame
+            # Visualize the results on the frame using the plot method
             annotated_frame = results[0].plot(line_width=2, font_size=18)
 
             # Get the mask and class IDs for the detected objects
             masks = results[0].masks
+            # Convert the class IDs to a NumPy array and move it to the CPU
             class_ids = results[0].boxes.cls.cpu().numpy()
+            # List to store the 3D point clouds of the detected objects
             point_clouds = []
 
-            # Process each mask
+            # Process each mask -> masks.data is a tensor of shape (N, H, W) where N is the number of masks
             for i, mask in enumerate(masks.data):
+                # Convert the mask tensor to a NumPy array and move it to the CPU
                 mask = mask.cpu().numpy()
                 mask = erode_mask(mask, iterations=1)
+                # Get the indices of the non-zero elements in the mask
                 mask_indices = np.argwhere(mask > 0)
 
+                # Convert the ZED depth map to a PyTorch tensor and move it to the GPU
                 depth_map = torch.from_numpy(zed_depth_np).to(device)
 
+                # Calculate 3D points from the mask indices and depth map
                 with torch.cuda.amp.autocast():
-                    points_3d = get_3d_points_torch(mask_indices, depth_map, cx, cy, fx, fy)
+                    points_3d = get_3d_points_torch(mask_indices, depth_map, cx, cy, fx, fy) # Returns a tensor of shape (N, 3) "List of 3D points"
 
+                # Check if any 3D points are returned
                 if points_3d.size(0) > 0:
                     point_cloud_np = points_3d.cpu().numpy()
                     point_clouds.append(point_cloud_np)
-
-                    # Calculate the centroid of the point cloud
-                    centroid = calculate_centroid(point_cloud_np)
                     class_id = int(class_ids[i])
-                    print(f"Class ID: {class_id}, CentroidXYZ-Coords: {centroid} in Camera Frame 1")
-
+                    print(f"Class ID: {class_id} ({class_names[class_id]}) in Camera Frame 1")
 
             # Visualize the 3D point clouds every 'update_frequency' frames
             if point_clouds and frame_count % update_frequency == 0:
@@ -213,11 +227,11 @@ def main():
                     # Randomly sample a fraction of the points
                     sampled_pc = random_sample_pointcloud(pc, fraction=0.05)  # Retain 5% of points
                     pcd = o3d.geometry.PointCloud(o3d.utility.Vector3dVector(sampled_pc))
-
                     class_id = int(class_ids[i])
                     color = np.array(color_map.get(class_id, [1, 1, 1])) / 255.0
                     pcd.paint_uniform_color(color)
 
+                    # TODO: Check if the transformation matrix is correct
                     # Transformation matrix for 180° rotation around the x-axis
                     transformation_matrix = np.array([[1, 0, 0, 0],
                                                       [0, -1, 0, 0],
@@ -228,6 +242,7 @@ def main():
                     vis.add_geometry(pcd)
 
                 vis.add_geometry(coordinate_frame)
+                vis.add_geometry(robot_base_frame)
                 vis.poll_events()
                 vis.update_renderer()
 
@@ -244,7 +259,6 @@ def main():
                     captured_point_clouds.append(pcd)
                 captured_point_clouds.append(coordinate_frame)
                 o3d.visualization.draw_geometries(captured_point_clouds)
-
 
             frame_count += 1
 
@@ -267,7 +281,7 @@ def main():
 
             key = cv2.waitKey(1)
 
-    # Close the camera and exit
+    # If q is pressed the loop will break and the camera will be closed
     zed.close()
     vis.destroy_window()
 
